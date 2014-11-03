519bf885f8ad1b200282a21420b03906958bb6d7
ChadRyan <chad.w.ryan@vanderbilt.edu>
2014-11-03 03:09:35 -0600

    A basic Event is created now based only on the sport and description as was discussed in our first design

--- PickupSportsAndroidProject/app/src/main/java/com/pickupsports/myapplication/AddEventDialog.java
+++ PickupSportsAndroidProject/app/src/main/java/com/pickupsports/myapplication/AddEventDialog.java
@@ -5,9 +5,15 @@ import android.content.Context;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 
+import com.pickupsports.repository.Event;
 
+/**
+ * AddEventDialog-
+ * used to create the dialog used to create a new pickup sports event
+ */
 public class AddEventDialog extends Dialog {
 
     public AddEventDialog(Context context) {
@@ -15,18 +21,19 @@ public class AddEventDialog extends Dialog {
         this.setContentView(R.layout.add_event_dialog);
         this.setTitle(context.getResources().getString(R.string.event_dialog_title));
 
-
-        Spinner spinner = (Spinner) findViewById(R.id.spinnerSports);
+        final EditText notes = (EditText) findViewById(R.id.EditTextAddNotes);
+        final Spinner spinner = (Spinner) findViewById(R.id.spinnerSports);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(context,
                 R.array.sports, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
 
-
         Button cancel = (Button) findViewById(R.id.buttonCancelAdding);
         cancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                //Event newEvent = new Event(null,)
                 AddEventDialog.this.dismiss();
             }
         });
@@ -36,8 +43,23 @@ public class AddEventDialog extends Dialog {
         addEvent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String sport = "basketball";
+                sport = spinner.getSelectedItem().toString();
+                Event newEvent = createBasicEvent(sport, notes.getText().toString());
                 AddEventDialog.this.dismiss();
             }
         });
+    }
+
+    /**
+     * createBasicEvent-
+     * private helper method used to create basic events
+     *
+     * @param sport       - the sport that is selected on the spinner
+     * @param description - the description of the event entered in the editText
+     * @return - the created event
+     */
+    private Event createBasicEvent(String sport, String description) {
+        return new Event(null, sport, description, 100, null);
     }
 }
