26b6e189ca10acf7cdd0098b1f18faa83d90539b
Clark Perkins <r.clark.perkins@gmail.com>
2014-11-04 14:50:29 -0600

    Removed the bad part of application.java

--- Server/src/main/java/com/pickupsports/Application.java
+++ Server/src/main/java/com/pickupsports/Application.java
@@ -4,23 +4,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pickupsports.json.ResourcesMapper;
 import com.pickupsports.repository.Event;
 import com.pickupsports.repository.EventRepository;
-import com.sun.jdi.connect.Connector;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.boot.context.embedded.tomcat.TomcatConnectorCustomizer;
-import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
-import java.io.File;
 
 /**
  * Created by clarkperkins on 10/23/14.
@@ -79,70 +70,6 @@ public class Application extends RepositoryRestMvcConfiguration {
     protected void configureRepositoryRestConfiguration(
             RepositoryRestConfiguration config) {
         config.exposeIdsFor(Event.class);
-    }
-
-    // This version uses the Tomcat web container and configures it to
-    // support HTTPS. The code below performs the configuration of Tomcat
-    // for HTTPS. Each web container has a different API for configuring
-    // HTTPS.
-    //
-    // The app now requires that you pass the location of the keystore and
-    // the password for your private key that you would like to setup HTTPS
-    // with. In Eclipse, you can set these options by going to:
-    //    1. Run->Run Configurations
-    //    2. Under Java Applications, select your run configuration for this app
-    //    3. Open the Arguments tab
-    //    4. In VM Arguments, provide the following information to use the
-    //       default keystore provided with the sample code:
-    //
-    //       -Dkeystore.file=src/main/resources/private/keystore -Dkeystore.pass=changeit
-    //
-    //    5. Note, this keystore is highly insecure! If you want more securtiy, you
-    //       should obtain a real SSL certificate:
-    //
-    //       http://tomcat.apache.org/tomcat-7.0-doc/ssl-howto.html
-    //
-    @Bean
-    EmbeddedServletContainerCustomizer containerCustomizer(
-            @Value("${keystore.file}") String keystoreFile,
-            @Value("${keystore.pass}") final String keystorePass)
-            throws Exception {
-
-
-        // This is boiler plate code to setup https on embedded Tomcat
-        // with Spring Boot:
-
-        final String absoluteKeystoreFile = new File(keystoreFile)
-                .getAbsolutePath();
-
-        return new EmbeddedServletContainerCustomizer() {
-            @Override
-            public void customize(ConfigurableEmbeddedServletContainer container) {
-                TomcatEmbeddedServletContainerFactory tomcat = (TomcatEmbeddedServletContainerFactory) container;
-                tomcat.addConnectorCustomizers(new TomcatConnectorCustomizer() {
-
-                    @Override
-                    public void customize(Connector connector) {
-                        connector.setPort(8443);
-                        connector.setSecure(true);
-                        connector.setScheme("https");
-
-                        Http11NioProtocol proto = (Http11NioProtocol) connector
-                                .getProtocolHandler();
-                        proto.setSSLEnabled(true);
-
-                        // If you update the keystore, you need to change
-                        // these parameters to match the keystore that you generate
-                        proto.setKeystoreFile(absoluteKeystoreFile);
-                        proto.setKeystorePass(keystorePass);
-                        proto.setKeystoreType("JKS");
-                        proto.setKeyAlias("tomcat");
-
-                    }
-                });
-            }
-
-        };
     }
 
 }
