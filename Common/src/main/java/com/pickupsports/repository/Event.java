83d39a3cc7c11af3c4b0b0daddb64d1cd96206b2
smoreilly <smoreilly112@gmail.com>
2014-12-02 23:54:10 -0600

    Added constructor from string for shared preff

--- Common/src/main/java/com/pickupsports/repository/Event.java
+++ Common/src/main/java/com/pickupsports/repository/Event.java
@@ -65,8 +65,19 @@ public class Event {
         super();
     }
 
-    public Event(String x){super();}
-    
+    public Event(String x){
+        String[] array = x.split("\t");
+        eventName = array[0];
+        sport = array[1];
+        description = array[2];
+        attendance = Long.parseLong(array[3]);
+        skillLevel = array[4];
+        equipment = array[5];
+        location = array[6];
+        //time =new Time(array[7]);
+        free = Boolean.parseBoolean(array[8]);
+    }
+
     public Event(String eventName, String sport, String description, long maxAttendance,
                  String skillLevel, String equipment, String location,
                  Date time, boolean free) {
@@ -240,8 +251,8 @@ public class Event {
 
     @Override
     public String toString() {
-        return eventName + " / " + sport + " / " + description + " / " + attendance + " / " + skillLevel + " / "
-                + equipment + " / " + location + " / " + time + " / " + free;
+        return eventName + "\t" + sport + "\t" + description + "\t" + attendance + "\t" + skillLevel + "\t"
+                + equipment + "\t" + location + "\t" + time + "\t" + free;
     }
 }
 
